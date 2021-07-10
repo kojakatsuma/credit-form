853fd1255333bdf71376a17111f5ddecdc4d8ee8
kojakatsuma <katsuma.koja@ubiregi.com>
2021-07-10 14:59:41 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react'
+import './App.css'
 
 function App() {
+  const form = useRef<HTMLFormElement | null>(null)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form ref={form} method="POST">
+        <div>
+          <label htmlFor="cc-number">クレジットカード番号</label>
+          <input
+            maxLength={16}
+            type="tel"
+            name="cc-number"
+            id="cc-number"
+            autoComplete="cc-number"
+          />
+        </div>
+        <div>
+          <label htmlFor="cc-exp-month">月</label>
+          <input type="tel" id="cc-exp-month" autoComplete="cc-exp-month" />
+        </div>
+        <div>
+          <label htmlFor="cc-exp-year">年</label>
+          <input
+            type="tel"
+            id="cc-exp-year"
+            name="cc-exp-year"
+            autoComplete="cc-exp-year"
+          />
+        </div>
+        <div>
+          <label htmlFor="cc-csc">cc</label>
+          <input type="tel" name="cc-csc" id="cc-csc" />
+        </div>
+        <button type="submit">送信</button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
